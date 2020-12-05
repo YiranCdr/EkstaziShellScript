745301247369af6ddcaad418e0abfec60651fa89
yiran <yiransucdr@utexas.edu>
2020-12-05 01:47:44 -0600

    Initial version of ekstazi eval script

---
+++ Res/ekstaziInsertion.py
@@ -0,0 +1,55 @@
+import xml.etree.ElementTree as ET
+import codecs
+import sys
+
+ekstaziPluginStr = "<plugin>" \
+                   "<groupId>org.ekstazi</groupId>" \
+                   "<artifactId>ekstazi-maven-plugins</artifactId>" \
+                   "<version>5.3.0</version>" \
+                   "<executions>" \
+                   "<execution>" \
+                   "<id>ekstazi</id>" \
+                   "<goals>" \
+                   "<goal>select</goal>" \
+                   "</goals>" \
+                   "</execution>" \
+                   "</executions>" \
+                   "</plugin>"
+ekstaziPluginsStr = "<plugins><plugin>" \
+                    "<groupId>org.ekstazi</groupId>" \
+                    "<artifactId>ekstazi-maven-plugins</artifactId>" \
+                    "<version>5.3.0</version>" \
+                    "<executions>" \
+                    "<execution>" \
+                    "<id>ekstazi</id>" \
+                    "<goals>" \
+                    "<goal>select</goal>" \
+                    "</goals>" \
+                    "</execution>" \
+                    "</executions>" \
+                    "<build></plugin></plugins>"
+ekstaziBuildStr = "<build><plugins><plugin>" \
+                  "<groupId>org.ekstazi</groupId>" \
+                  "<artifactId>ekstazi-maven-plugins</artifactId>" \
+                  "<version>5.3.0</version>" \
+                  "<executions>" \
+                  "<execution>" \
+                  "<id>ekstazi</id>" \
+                  "<goals>" \
+                  "<goal>select</goal>" \
+                  "</goals>" \
+                  "</execution>" \
+                  "</executions>" \
+                  "</plugin></plugins></build>"
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2
+    destName = sys.argv[1]
+    tree = ET.parse(destName)
+    tag = tree.getroot().tag[:-7]
+    ET.register_namespace("", tag[1:-1])
+    ekstaziPluginTree = ET.fromstring(ekstaziPluginStr)
+    tree.find(tag + "build/" + tag + "plugins").append(ekstaziPluginTree)
+    with codecs.open(destName, "w", encoding="utf8") as f:
+        f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+    tree.write(open(destName, 'ab'))
---
+++ RunPerProj.sh
@@ -0,0 +1,169 @@
+# WARNING: PLEASE MAKE SURE THAT COMMIT ORIGIN/MASTER IS WORKIGN FOR ALL EKSTAZI EXE. 
+# To check the pure version (without debugging any info) of log file, appliy this regex to your sublimeText by pressing ctrl + H.
+# (^extendedLoad:.*\n)|(^file:/mnt/c/MyProj/EkstaziShellScript/javapoet/target.*.class.*\n)|(jar:file:/home/yiran/.m2/repository.*\n)|(^extendedSave:.*\n)|(^/mnt/c/MyProj/EkstaziShellScript/javapoet/.ekstazi\n)|(^clz.*\n)|(^\[file:/mnt/c/MyProj/EkstaziShellScript.*\n)
+
+# Param1: N. Roll back N commits.
+rollBackN() {
+    # clean untracked files like .ekstazi/*
+    git clean -fd
+    # Force to checkout, since we may modify pom.xml
+    git checkout -f HEAD~$1
+}
+# Param1: n, current head location
+# Safe Method - Unit ready. 
+rollBackLastValid() {
+    n=$1
+    output=''
+    while ((${#output} == 0))
+    do
+        ((n=$n + 1))
+        output=$(git diff  --name-only HEAD HEAD~$n -- *.java)
+    done
+    rollBackN $n
+    return $n
+}
+
+# Param1: projectName
+# Param2: logFileName
+# Param3: timeFileName
+# Param5: ifExecuteWithoutEkstazi true/false
+# Param5: ifExecuteOriginalEkstazi true/false
+# Param6: ifExecuteOurEkstazi true/false
+run() {
+  projectName=$1
+  logFileName=$2
+  timeFileName=$3
+  ifExecuteWithoutEkstazi=$4
+  ifExecuteOriginalEkstazi=$5
+  ifExecuteOurEkstazi=$6
+  cp pom.xml ../Res/$1/pom.xml
+  printf "$(git rev-parse HEAD)," >> $timeFileName
+
+  # Run without Ekstazi
+  if [[ $ifExecuteWithoutEkstazi == true ]]
+  then
+    start_time=`date +%s`
+    mvn clean install >> $logFileName
+    mvnExitCode=$?
+    end_time=`date +%s`
+    printf `expr $end_time - $start_time` >> $timeFileName
+    printf "," >> $timeFileName 
+    # Check if maven build successfully
+    if [[ "$mvnExitCode" -ne 0 ]] 
+    then
+      # Print out exe time if the following 2 steps are not exed at all. 
+      printf "0,0," >> $timeFileName
+      return 1
+    fi
+  else
+    printf "0," >> $timeFileName
+  fi
+
+  # Run with original Ekstazi
+  if [[ $ifExecuteOriginalEkstazi == true ]]
+  then
+    # Modifiy pom.xml to load ekstazi
+    python ../Res/ekstaziInsertionOriEkstazi.py pom.xml
+    prevEkstaziFolder="../Res/${projectName}/.ekstazi"
+    if [[ -d $prevEkstaziFolder ]]
+    then
+      cp -r $prevEkstaziFolder .ekstazi
+    fi
+    start_time=`date +%s`
+    mvn clean install >> $logFileName
+    mvnExitCode=$?
+    end_time=`date +%s`
+    printf `expr $end_time - $start_time` >> $timeFileName
+    printf "," >> $timeFileName 
+    if [[ "$mvnExitCode" -ne 0 ]]
+    then
+      # Print out exe time if the following 1 step are not exed at all. 
+      printf "0," >> $timeFileName
+      return 1
+    fi
+    # if build success, rm the original .ekstazi folder, copy-past the new .ekstazi folder
+    rm -rf $prevEkstaziFolder
+    cp -r .ekstazi $prevEkstaziFolder
+  else
+    printf "0," >> $timeFileName
+  fi
+
+  # Run with our Ekstazi
+  if [[ $ifExecuteOurEkstazi == true ]]
+  then
+    cp ../Res/$1/pom.xml pom.xml
+    python ../Res/ekstaziInsertion.py pom.xml
+    # prevEkstaziFolder="../Res/${projectName}/.ekstazi"
+    # rm -r -f .ekstazi
+    # if [[ -d $prevEkstaziFolder ]]
+    # then
+    #   cp -r $prevEkstaziFolder .ekstazi
+    # fi
+    start_time=`date +%s`
+    mvn clean install >> $logFileName
+    mvnExitCode=$?
+    end_time=`date +%s`
+    printf `expr $end_time - $start_time` >> $timeFileName
+    printf "," >> $timeFileName 
+    if [[ "$mvnExitCode" -ne 0 ]]
+    then
+      return 1
+    fi
+  else
+    printf "0," >> $timeFileName
+  fi
+
+  return 0
+}
+# Param1: projectName
+main() {
+  projectName=$1
+  cd $projectName
+  git clean -fd
+  git checkout -f origin/master
+  time=$(date "+%Y%m%d-%H%M%S")
+  # Mainly record maven output
+  logFileName="../Res/$projectName/$time.log"
+  # a csv file: commitSHA,RunWithoutEkstaziInSec,RunWithMilosEkstaziInSec,RunWithOurEkstaziInSec,ExitCode,
+  timeFileName="../Res/$projectName/$time.time.log"
+  # Add a title for timeFileName
+  printf "commitSHA,RunWithoutEkstaziInSec,RunWithMilosEkstaziInSec,RunWithOurEkstaziInSec,ExitCode,\n" >> $timeFileName
+  # If folder exists, -p will skip this mkdir.
+  mkdir -p ../Res/$projectName
+  # Remove the initial .ekstazi folder, if exist.
+  rm -rf ../Res/$projectName/.esktazi
+  lastValidSha=$(git rev-parse HEAD)
+  round=20
+  run $projectName $logFileName $timeFileName true true false
+  # If the above line fails, -1 will be added as the exitcode. Otherwise, 0. 
+  if [[ "$?" -ne 0 ]]
+  then
+    printf "1,\n" >> $timeFileName
+  else
+    printf "0,\n" >> $timeFileName 
+  fi
+  # for the residual 19 reversions - 
+  rollbackNum=0
+  while (($round>=2))
+  do
+    ((round=$round-1))
+    rollBackLastValid $rollbackNum
+    rollbackNum=$?
+    run $projectName $logFileName $timeFileName true true false
+    if [[ "$?" -ne 0 ]]
+    then
+      printf "1,\n" >> $timeFileName
+      ((round=$round+1))
+      git clean -fd
+      git checkout -f $lastValidSha
+    else
+      printf "0,\n" >> $timeFileName 
+      lastValidSha=$(git rev-parse HEAD)
+      rollbackNum=0
+    fi
+  done
+}
+# Param1: project name
+# main $1
+
+main javapoet
