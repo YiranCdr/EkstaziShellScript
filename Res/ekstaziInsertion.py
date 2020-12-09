a4e79956b106b47f0e43a43d827a3038a804d595
PengfeiRan <pengfei.ran@utexas.edu>
2020-12-08 22:14:56 -0600

    add postgresql mongodb dependencies

--- Res/ekstaziInsertion.py
+++ Res/ekstaziInsertion.py
@@ -42,6 +42,18 @@ ekstaziBuildStr = "<build><plugins><plugin>" \
                   "</executions>" \
                   "</plugin></plugins></build>"
 
+sqlDependencyStr =  "<dependency>" \
+                    "<groupId>org.postgresql</groupId>" \
+                    "<artifactId>postgresql</artifactId>" \
+                    "<version>42.2.10</version>" \
+                    "</dependency>"
+
+mongoDependencyStr =    "<dependency>" \
+                        "<groupId>org.mongodb</groupId>" \
+                        "<artifactId>mongodb-driver-sync</artifactId>" \
+                        "<version>3.12.7</version>" \
+                        "</dependency>"
+
 if __name__ == "__main__":
     assert len(sys.argv) == 2
     destName = sys.argv[1]
@@ -50,6 +62,10 @@ if __name__ == "__main__":
     ET.register_namespace("", tag[1:-1])
     ekstaziPluginTree = ET.fromstring(ekstaziPluginStr)
     tree.find(tag + "build/" + tag + "plugins").append(ekstaziPluginTree)
+    sqlDependencyTree = ET.fromstring(sqlDependencyStr)
+    tree.find(tag + "dependencies").append(sqlDependencyStr)
+    mongoDependencyTree = ET.fromstring(mongoDependencyStr)
+    tree.find(tag + "dependencies").append(mongoDependencyStr)
     with codecs.open(destName, "w", encoding="utf8") as f:
         f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
     tree.write(open(destName, 'ab'))
